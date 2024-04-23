b4ebf3c49beda6aa83953086d13517ccac1614ee
LarissaS10 <larissarochamoreti10@gmail.com>
2024-04-22 22:18:02 -0300

    Nova alteração com adição do prompt

--- FirstApp/script.js
+++ FirstApp/script.js
@@ -1,14 +1,28 @@
+//constantes
+const SALARIO_ATE_20 = 1000
+const SALARIO_ACIMA_20 = 2000
+
 //input
-anoNascimento = 2001
-nome = "Larissa"
+anoNascimento = parseInt(prompt("Informe o seu ano de nascimento"))
+nome = prompt("Informe seu nome")
+salarioBase = parseFloat(prompt("Informe seu salário base"))
+gratificacao = parseFloat(prompt("Informe a gratificação"))
+bonus = parseFloat(prompt("Informe o bônus"))
+desconto = parseFloat(prompt("Informe o desconto"))
+
+salarioLiquido = 0
+adicional = 0
 
 //processamento
 hoje = new Date()
 anoAtual = hoje.getFullYear()
 idade = anoAtual - anoNascimento
-mensagem = "Sou " + nome + " e tenho " + idade + " anos"
+
+adicional = SALARIO_ATE_20
+
+salarioLiquido = salarioBase + gratificacao + bonus - desconto + adicional
 
 //output
+mensagem = "Sou " + nome + ", tenho " + idade + " anos e ganho R$" + salarioLiquido
 alert(mensagem)
-
 
